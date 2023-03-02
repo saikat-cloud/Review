2fd98104a46ba100903d4898de7219035d5daa48
saikat <saikat.mondal.dev@gmail.com>
2023-03-02 19:21:41 +0600

    if else

---
+++ ifelse.py
@@ -0,0 +1,24 @@
+
+# # if statement" is written by using the if keyword
+# a = 13
+# b = 12
+# if a > b:
+#     print("a is greater then b")
+
+
+# a = 12
+# b = 13
+# if a > b:
+#     print("a is greater then b")
+# elif a == b:
+#     print("a is equal b")
+# else:
+#     print("b is greater then a")
+
+
+#The "and" "or" "not" keyword is a logical operator, and is used to combine conditional statements
+a = 5
+b = 6
+c = 7
+if a < b and b < c:
+    print("both condition are true")
